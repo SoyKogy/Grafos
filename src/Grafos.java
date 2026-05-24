198c624e09e40fbd0c4f4522183acab9b6125f26
SoyKogy <173300376+SoyKogy@users.noreply.github.com>
2026-05-24 17:48:57 -0500

    menu fixes

--- src/Grafos.java
+++ src/Grafos.java
@@ -1,3 +1,21 @@
 public class Grafos {
 
+    public static void crearGrafo(String nodosString, int tipo) {
+        String[] nodos = nodosString.split(",");
+
+        /* tipo 1 = no dirigido */
+        if (tipo == 1) {
+            for (int i = 0; i < nodos.length; i++) {
+                for (int j = 1; j < nodos.length; j++) {
+                    
+                }
+            }
+            
+        
+        /* tipo 2 = dirigido */
+        } else if (tipo == 2) {
+            System.out.println("Has seleccionado la opción 2: Crear un grafo dirigido");
+            // Aquí puedes agregar el código para crear un grafo dirigido
+        }
+    }
 }
--- src/MenuGrafos.java
+++ src/MenuGrafos.java
@@ -8,16 +8,18 @@ public class MenuGrafos {
         String nodosString = new String();
 
         do {
-            menu();
+            opcion = menu(0);            
             switch(opcion) {
                 
                 case 1:
                     nodosString = JOptionPane.showInputDialog("Ingrese los nodos del grafo, separados por comas: ");
-                    // Aquí puedes agregar el código para crear un grafo no dirigido
+                    Grafos.crearGrafo(nodosString, 1);
+                    menuGrafos();
                     break;
                 case 2:
-                    System.out.println("Has seleccionado la opción 2: Crear un grafo dirigido");
-                    // Aquí puedes agregar el código para crear un grafo dirigido
+                    nodosString = JOptionPane.showInputDialog("Ingrese los nodos del grafo, separados por comas: ");
+                    Grafos.crearGrafo(nodosString, 2);
+                    menuGrafos();
                     break;
                 case 0:
                     break;
@@ -28,12 +30,54 @@ public class MenuGrafos {
         } while (opcion != 0);
     }
 
-    public static void menu() {
-        System.out.println("======= Grafos =======" +
-                            "1. Crear un grafo no dirigido\n" +
-                            "2. Crear un grafo dirigido\n\n" +
-                            "0. Salir"
-        );
+    public static int menu(int opcion) {
 
+        int opcionUsuario = 0;
+        if (opcion == 0) {
+            opcionUsuario = Integer.parseInt(JOptionPane.showInputDialog(null, "======= Grafos =======" +
+                                "1. Crear un grafo no dirigido\n" +
+                                "2. Crear un grafo dirigido\n\n" +
+                                "0. Salir"
+            ));
+        } else if (opcion == 1) {
+            opcionUsuario = Integer.parseInt(JOptionPane.showInputDialog(null, "======= Grafos =======" +
+                            "1. Mostrar grafo\n" +
+                            "2. Mostrar matriz de adyacencia\n" +
+                            "3. Mostrar lista de adyacencia\n" +
+                            "4. Mostrar matriz de incidencia\n\n" +
+                            "0. Volver a la creación de grafos"
+            ));
+        }
+
+        return opcionUsuario;
+    }
+
+    public static void menuGrafos() {
+        
+        int opcion = 0;
+        do {
+            opcion = menu(1);
+                switch(opcion) {
+                
+                case 1:
+                    // mostrar grafo
+                    break;
+                case 2:
+                    // mostrar matriz de adyacencia
+                    break;
+                case 3:
+                    // mostrar lista de adyacencia
+                    break;
+                case 4:
+                    // mostrar matriz de incidencia
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+                    break;
+            }
+        } while (opcion != 0);
+        
     }
 }
